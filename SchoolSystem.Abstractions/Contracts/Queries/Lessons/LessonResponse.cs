43d84a8719fd42ddf53309184f896a7de122d048
MaxDMT16 <maxdmt1638@gmail.com>
2018-06-03 15:01:59 +0300

    add lesson by id query

---
+++ SchoolSystem.Abstractions/Contracts/Queries/Lessons/LessonResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SchoolSystem.Abstractions.CQRS.Contracts;
+using SchoolSystem.Abstractions.Enums;
+
+namespace SchoolSystem.Abstractions.Contracts.Queries.Lessons
+{
+    public class LessonResponse : IQueryResult
+    {
+        public Guid Id { get; set; }
+
+        public Guid TeacherId { get; set; }
+        
+        public Guid GroupId { get; set; }
+
+        public Subject Subject { get; set; }
+    }
+}
